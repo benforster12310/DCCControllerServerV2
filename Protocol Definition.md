daaabca26a21079b45083357ef6bed613132d37c
benforster12310 <forster.ben66@gmail.com>
2023-10-05 08:19:39 +0100

    Update

---
+++ Protocol Definition.md
@@ -0,0 +1,58 @@
+# Protocol Definition
+
+
+
+## Data Packets Will Contain
+
+- Global Emergency Stop
+- Global Track Short
+- The SIMPLE locomotive address 0-15
+- Emergency Stop
+- The Current Speed
+- The Current Direction
+- Which Functions Are On Starting At F0
+
+An example with No Faults, A loco Address of 7, current speed (positive only) 22, going forwards and with functions 1,4,7 on
+
+> 0000111010110101001001
+
+### Indexes
+
+- **[0]** - Global Emergency Stop. 1 if emergency stop. 0 if not
+- **[1]** - Global Track Short. 1 if track shorted. 0 if not - (host to controller only)
+- **[2]** - Loco Address 4th Bit (8)
+- **[3]** - Loco Address 3rd Bit (4)
+- **[4]** - Loco Address 2nd Bit (2)
+- **[5]** - Loco Address 1st Bit (1)
+- **[6]** - Emergency Stop. 1 if emergency stop. 0 if not (controller to host only)
+- **[7]** - Current Speed 5th Bit (16)
+- **[8]** - Current Speed 4th Bit (8)
+- **[9]** - Current Speed 3th Bit (4)
+- **[10]** - Current Speed 2nd Bit (2)
+- **[11]** - Current Speed 1st Bit (1)
+- **[12]** - Direction Forwards. 1 if going forwards. 0 if going backwards
+- **[13]** - Custom Button 1. 1 if on. 0 if off
+- **[14]** - Custom Button 2. 1 if on. 0 if off
+- **[15]** - Custom Button 3. 1 if on. 0 if off
+- **[16]** - Custom Button 4. 1 if on. 0 if off
+- **[17]** - Custom Button 5. 1 if on. 0 if off
+- **[18]** - Custom Button 6. 1 if on. 0 if off
+- **[19]** - Custom Button 7. 1 if on. 0 if off
+- **[20]** - Custom Button 8. 1 if on. 0 if off
+- **[21]** - Custom Button 9. 1 if on. 0 if off
+- **[22]** - Custom Button 10. 1 if on. 0 if off
+- **[23]** - Custom Button 11. 1 if on. 0 if off
+- **[24]** - Custom Button 12. 1 if on. 0 if off
+- **[25]** - Custom Button 13. 1 if on. 0 if off
+- **[26]** - Custom Button 14. 1 if on. 0 if off
+- **[27]** - Custom Button 15. 1 if on. 0 if off
+
+> more functions could be added if necessary however it is unlikely that the hardware will be there to use the extra functions nor are they needed in most cases
+
+### Error Definitions
+
+- **99** - Disconnetion Error
+- **88** - General Error
+- **77** - Not Connecting Error
+- **66** - Emergency Stop
+- **55** - Track Short
--- server.mjs
+++ server.mjs
@@ -2,13 +2,131 @@
 
 // Import Modules
 import { WebSocketServer } from 'ws';
-import { read, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 
-var LocosObject = {}
+// define main variables
+var LocosObject = {};
+var globalEmergencyStop = false;
+var globalTrackShort = false;
 
 // 1 - Load The Webserver
 const wss = new WebSocketServer({ port: 81, path:"/ws" });
 
-// 2 - Go through the locos.json file
+// 2 - Load and Parse locos.json file
 var locosJson = JSON.parse(readFileSync("./locos.json"));
 
+// 3 - Go througn the locosJson file and update the locosObject
+for(var key in locosJson) {
+    var locoCustomButtons = [];
+    for(var key1 in locosJson[key]["cB"]) {
+        locoCustomButtons[parseInt(key1) -1] = 0;
+    }
+
+    LocosObject[locosJson[key]["sA"]] = {}
+
+    LocosObject[locosJson[key]["sA"]]["string"] = "00" + locosJson[key]["sA"] + "0000001" + locoCustomButtons.join("");
+
+    // then put the cB key and property into the same thing
+    LocosObject[locosJson[key]["sA"]]["cB"] = locosJson[key]["cB"];
+
+    console.log(LocosObject)
+    
+}
+
+
+
+// broadcastAllLocos
+
+function broadcastAllLocos(wss) {
+    // go through all loco objects
+    for(key in LocosObject) {
+        // 1 - Then get the binary string
+        var string = LocosObject[key]["string"];
+
+        // 2 - Broadcast to all devices
+        wss.clients.forEach(function each(client) {
+            client.send(string, {binary: true});
+        })
+    }
+}
+
+
+// checkEmergencyStop
+function checkEmergencyStop() {
+
+}
+
+// updateLoco
+
+function updateLoco(serverAddress) {
+    console.log("Call To Update Loco: " + serverAddress);
+}
+
+
+// - Wait For Connections To The Websocket
+wss.on("connection", function connection(ws) {
+    console.log("Connected To A Client");
+
+    // then as there is a new connection we need to broadcast all of the locos again so it can update itself
+    broadcastAllLocos(wss);
+
+    // then wait for messages from the client
+    ws.on("message", function message(data) {
+        // then read the buffer
+        var dataJson = data.toJSON();
+
+        var binary = dataJson.data;
+
+        console.log(dataJson)
+
+        // then check parts
+
+        // 1 - check the global emergency stop
+        globalEmergencyStop = binary[0];
+
+        checkEmergencyStop();
+
+        // 2 - check the address bits
+        var address = [];
+        address[0] = binary[2];
+        address[1] = binary[3];
+        address[2] = binary[4];
+        address[3] = binary[5];
+
+        // 3 - then put the binary as a string into the LocosObject
+        var addressString = address.join("");
+        var dataString = binary.join("");
+
+        console.log(address);
+        console.log(addressString);
+
+        LocosObject[addressString]["string"] = dataString;
+
+        updateLoco(addressString);
+
+        // then go through the custom buttons and check if any of them are momentary
+
+        for(var i = 13; i < binary.length; i++) {
+            // then check if the button is on
+            if(binary[i] == 1) {
+                // then it is on so check if it is momentary
+                if(LocosObject[addressString]["cB"][i-12]["m"] == true) {
+                    // then set a timer to turn it off
+                    setTimeout(function() {
+                        // retrieve the current stored variable
+                        var str = LocosObject[addressString]["string"];
+
+                        // then go to the i'th bit
+                        str[i] = 0;
+
+                        LocosObject[addressString]["string"] = str;
+
+                    }, 500);
+                }
+            }
+        }
+
+        console.log(str);
+        
+    })
+})
